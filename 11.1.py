5987c8bbe2a973a8f0499385286816e0b1565240
larsgorter <larsgorter2@gmail.com>
2020-04-19 18:25:43 +0200

    11.1 exercise with reflect x and distance

--- 11.1.py
+++ 11.1.py
@@ -7,6 +7,9 @@ class Point:
     def __str__(self):
         return "({0}, {1})".format(self.x, self.y)
 
+    def get_distance(self, target=0):
+        return ((self.x - target.x) ** 2 + (self.y - target.y) ** 2) ** 0.5
+
     def reflect_x(self):
         return Point(self.x, -self.y)
 
